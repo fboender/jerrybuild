d7a0fe9a348317feeea0f925ff1f7e8683f6e259
Ferry Boender <ferry.boender@gmail.com>
2016-04-04 08:39:31 +0200

    Separate job info into its own class.

--- src/build_queue.py
+++ src/build_queue.py
@@ -9,6 +9,68 @@ import json
 import subprocess
 from tools import mkdir_p
 
+class Job:
+    def __init__(self, project, env, cmd, work_dir=None, id=None):
+        self.project = project
+        self.env = env
+        self.set_cmd(cmd, work_dir)
+        if id is not None:
+            self.id = id
+        else:
+            self.id = uuid.uuid4().hex
+        self.status = None
+        self.exit_code = None
+        self.stdout = None
+        self.stderr = None
+
+    def set_cmd(self, cmd, work_dir=None):
+        if work_dir is not None:
+            work_dir = os.path.realpath(work_dir)
+            if cmd.startswith('/'):
+                cmd = os.path.realpath(cmd)
+            else:
+                cmd = os.path.realpath(os.path.join(work_dir, cmd))
+        else:
+            if cmd.startswith('/'):
+                work_dir = os.path.realpath(os.path.dirname(cmd))
+                cmd = os.path.realpath(cmd)
+            else:
+                work_dir = os.path.realpath(os.curdir)
+                cmd = os.path.realpath(os.path.join(work_dir, cmd))
+
+        self.work_dir = work_dir
+        self.cmd = cmd
+
+    def set_status(self, status):
+        self.status = status
+
+    def run(self):
+        p = subprocess.Popen(self.cmd,
+                             cwd=self.work_dir,
+                             shell=True,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE,
+                             env=self.env)
+        self.stdout, self.stderr = p.communicate(input)
+        self.exit_code = p.returncode
+
+    def to_dict(self):
+        d = {
+            'project': self.project,
+            'env': self.env,
+            'cmd': self.cmd,
+            'work_dir': self.work_dir,
+            'id': self.id,
+            'status': self.status,
+            'exit_code': self.exit_code,
+            'stdout': self.stdout,
+            'stderr': self.stderr,
+        }
+        return d
+
+    def __repr__(self):
+        return "job '{}', project={}".format(self.id, self.project)
+
 
 class BuildQueue(threading.Thread):
     daemon = True
@@ -19,90 +81,51 @@ class BuildQueue(threading.Thread):
         threading.Thread.__init__(self)
         self.queue = Queue.Queue()
 
+    def run(self):
+        self.handle_queue()
+
     def make_status_dir(self):
         jobs_dir = os.path.join(self.status_dir, 'jobs')
         projects_dir = os.path.join(self.status_dir, 'projects')
         mkdir_p(jobs_dir)
         mkdir_p(projects_dir)
 
-    def run(self):
+    def put(self, job):
+        job.set_status('queued')
+        self.write_job_status(job)
+        self.queue.put(job)
+        logging.info("{}: queued".format(job))
+        return job.id
+
+    def handle_queue(self):
         logging.info("Build queue running")
         while True:
             job = self.queue.get()
             self.build(job)
 
-    def put(self, project, env, cmd, work_dir=None):
-        job = {
-            'project': project,
-            'id': uuid.uuid4().hex,
-            'env': env,
-            'cmd': cmd,
-            'work_dir': work_dir,
-        }
-        self.write_job_status(job, 'queued')
-        self.queue.put(job)
-        return job['id']
-
     def build(self, job):
-        self.write_job_status(job, 'starting')
+        job.set_status('running')
+        self.write_job_status(job)
+        logging.info("{}: starting".format(job))
 
-        job_id = job['id']
+        for k, v in job.env.items():
+            logging.debug('{}: env: {}={}'.format(job, k, v))
+        logging.debug("{}: executing '{}'".format(job, job.cmd))
 
-        logging.info("job {}: starting".format(job_id))
-        for k, v in job['env'].items():
-            logging.debug('job {}: env: {}={}'.format(job_id, k, v))
-        logging.debug("job {}: executing '{}'".format(job_id, job['cmd']))
+        job.run()
 
-        self.write_job_status(job, 'building')
+        job.set_status('done')
+        self.write_job_status(job)
+        logging.info("{}: done. Exit code = {}".format(job, job.exit_code))
 
-        if job['work_dir'] is not None:
-            cwd = os.path.realpath(job['work_dir'])
-            if job['cmd'].startswith('/'):
-                cmd = os.path.realpath(job['cmd'])
-            else:
-                cmd = os.path.realpath(os.path.join(cwd, job['cmd']))
-        else:
-            if job['cmd'].startswith('/'):
-                cwd = os.path.realpath(os.path.dirname(job['cmd']))
-                cmd = os.path.realpath(job['cmd'])
-            else:
-                cwd = os.path.realpath(os.curdir)
-                cmd = os.path.realpath(os.path.join(cwd, job['cmd']))
-
-        print cwd, cmd
-
-        p = subprocess.Popen(cmd, cwd=cwd, shell=True, stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE, env=job['env'])
-        stdout, stderr = p.communicate(input)
-        self.write_job_status(job, 'finished', exit_code=p.returncode,
-                              stdout=stdout, stderr=stderr)
-        logging.info("job {}: done. Exit code: {}".format(job_id, p.returncode))
-
+    def write_job_status(self, job):
         job_dir = os.path.join(self.status_dir, 'jobs')
-        job_path = os.path.join(job_dir, job['id'])
-        project_dir = os.path.join(self.status_dir, 'projects', job['project'])
-        latest_path = os.path.join(project_dir, 'latest')
-        try:
-            os.unlink(latest_path)
-        except OSError:
-            pass
-        os.symlink(job_path, latest_path)
-
-    def write_job_status(self, job, status, exit_code=None, stdout=None, stderr=None):
-        job_dir = os.path.join(self.status_dir, 'jobs')
-        job_path = os.path.join(job_dir, job['id'])
-        project_dir = os.path.join(self.status_dir, 'projects', job['project'])
-        project_path = os.path.join(project_dir, job['id'])
+        job_path = os.path.join(job_dir, job.id)
+        project_dir = os.path.join(self.status_dir, 'projects', job.project)
+        project_path = os.path.join(project_dir, job.id)
         mkdir_p(project_dir)
 
-        status = {
-            'project': job['project'],
-            'job_id': job['id'],
-            'status': status,
-            'exit_code': exit_code,
-            'stdout': stdout,
-            'stderr': stderr,
-        }
+        status = job.to_dict()
         with open(job_path, 'w') as f:
             json.dump(status, f)
         if not os.path.exists(project_path):
