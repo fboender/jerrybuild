e880d835632b9af60c56a19d649e5609d854cc61
Ferry Boender <ferry.boender@gmail.com>
2016-04-11 20:27:41 +0200

    Provider updates.

--- src/providers/github.py
+++ src/providers/github.py
@@ -5,13 +5,36 @@ import hashlib
 
 
 def normalize(request, project_name, config):
-    # Verify hash signature
-    body = request.body.read()
-    print body
-    hash = hmac.new(bytes('4bier4u2'), msg=body, digestmod=hashlib.sha1)
-
-    print hash.hexdigest()
     env = {}
+    body = request.body.read()
+    config_section = 'project:{}'.format(project_name)
+    secret = config.get(project_name, 'secret')
+
+    if request.headers['X-Github-Event'] != 'push':
+        raise NotImplementedError("Only push events are currently supported")
+
+    # Verify hash signature
+    hashtype, signature = self.headers['X-Hub-Signature'].split('=', 1)
+    if hashtype != 'sha1':
+        raise ValueError("Invalid hashtype received")
+
+    mac = hmac.new(secret, msg=data, digestmod=hashlib.sha1)
+    match = hmac.compare_digest(mac.hexdigest(), signature)
+    if not match:
+        raise ValueError("Invalid secret")
+
     env["event"] = request.headers['X-Github-Event']
+    env['repo_type'] = 'git'
+    env['repo_url'] = body['repository']['clone_url']
+    env['repo_url_ssh'] = body['repository']['ssh_url']
+    env['repo_url_http'] = body['repository']['clone_url']
+    env['ref'] = body['ref']
+    env['commit'] = body['after']
+
+    mail_to = []
+    mail_to.append(body['repository']['owner']['email'])
+    mail_to.append(body['pusher']['email'])
+    env['mail_to'] = ', '.join(mail_to)
+
     return env
 
--- src/providers/gogs.py
+++ src/providers/gogs.py
@@ -7,11 +7,15 @@ def normalize(request, project_name, config):
     env = {}
     body = json.load(request.body)
     config_section = 'project:{}'.format(project_name)
+    secret = config.get(config_section, 'secret')
 
-    # FIXME: Gogs doesn't implement body signing using hmac yet. See bug
+    if request.headers['X-Gogs-Event'] != 'push':
+        raise NotImplementedError("Only push events are currently supported")
+
+    # Gogs doesn't implement body signing using hmac yet. See bug
     # https://github.com/gogits/gogs/issues/2256. So we just compare the secret
     # for now, which is insecure.
-    if body['secret'] != config.get(config_section, 'secret'):
+    if body['secret'] != secret:
         raise ValueError("Invalid secret")
 
     env['event'] = request.headers['X-Gogs-Event']
