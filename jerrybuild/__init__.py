700d90ea8d4335950db95fd9478c5d1c15b30ecb
Ferry Boender <ferry.boender@gmail.com>
2020-10-05 09:06:53 +0200

    Version bump

--- jerrybuild/__init__.py
+++ jerrybuild/__init__.py
@@ -4,7 +4,7 @@ Init
 
 __METADATA__ = {
     "name": "jerrybuild",
-    "version": "9.99",
+    "version": "1.0.0-alpha",
     "author": "Ferry Boender",
     "author_email": "ferry.boender@gmail.com",
     "desc": "A lightweight build / CI (Continuous Integration) server.",
